b1722ecceeb73bd218457a5b0ab776ec8ce0f712
Johannes Schmid <johannes@schmid-company.de>
2024-10-06 21:01:41 +0200

    updated zammad to 6.3.0, bugfix

--- service.sh
+++ service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVICE_NAME="zammad"
-SERVICE_VERSION="v2.0"
+SERVICE_VERSION="v2.1"
 
 set -e
 
@@ -10,22 +10,22 @@ cd $SERVICE_DIR
 
 # VARIABLES
 set -o allexport
-VERSION=6.2
+VERSION=6.3.0
 IMAGE_REPO=ghcr.io/zammad/zammad
 
-ELASTICSEARCH_VERSION=8.8.0
+ELASTICSEARCH_VERSION=8.12.2
 
-MEMCACHE_VERSION=1.6.20-alpine
+MEMCACHE_VERSION=1.6.25-alpine
 MEMCACHE_SERVERS=zammad-memcached:11211
 
-POSTGRES_VERSION=15.3-alpine
+POSTGRES_VERSION=15.6-alpine
 POSTGRES_HOST=zammad-postgresql
 POSTGRES_PORT=5432
 POSTGRES_USER=zammad
 POSTGRES_PASS=zammad
 POSTGRES_DB=zammad_production
 
-REDIS_VERSION=7.0.5-alpine
+REDIS_VERSION=7.2.4-alpine
 REDIS_URL=redis://zammad-redis:6379
 
 NGINX_SERVER_SCHEME=https
@@ -49,12 +49,12 @@ cmd_exec-rails() {
 
 # ATTACHMENTS
 att_setup() {
-  mkdir -p $SERVICE_DIR/volumes/backup
-  mkdir -p $SERVICE_DIR/volumes/elasticsearch
-  mkdir -p $SERVICE_DIR/volumes/postgresql
-  mkdir -p $SERVICE_DIR/volumes/redis
-  mkdir -p $SERVICE_DIR/volumes/storage
-  mkdir -p $SERVICE_DIR/volumes/var
+  sudo mkdir -p $SERVICE_DIR/volumes/backup
+  sudo mkdir -p $SERVICE_DIR/volumes/elasticsearch
+  sudo mkdir -p $SERVICE_DIR/volumes/postgresql
+  sudo mkdir -p $SERVICE_DIR/volumes/redis
+  sudo mkdir -p $SERVICE_DIR/volumes/storage
+  sudo mkdir -p $SERVICE_DIR/volumes/var
   sudo chmod -R 777 $SERVICE_DIR/volumes
 }
 
